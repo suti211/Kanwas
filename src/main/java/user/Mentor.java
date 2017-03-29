4bbd82d363f00c437eb60f4f3f232859e2eeac23
redgyuf <faramirex070@gmail.com>
2017-03-29 13:47:55 +0200

    modified constructort according to user mods

--- src/main/java/user/Mentor.java
+++ src/main/java/user/Mentor.java
@@ -2,15 +2,15 @@ package user;
 
 public class Mentor extends User{
 
-	
-	
-	public Mentor(String name, String emailAddress) {
-		super(name, emailAddress);
+	public Mentor(String name, String emailAddress, String role, String password) {
+		super(name, emailAddress, role, password);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Mentor(String name, String emailAddress, String userName, String password) {
-		super(name, emailAddress, userName, password);
+	public Mentor(String name, String emailAddress, String role) {
+		super(name, emailAddress, role);
 		// TODO Auto-generated constructor stub
 	}
+
+	
 }
--- src/main/java/user/Student.java
+++ src/main/java/user/Student.java
@@ -2,15 +2,15 @@ package user;
 
 public class Student extends User{
 
-	
-	
-	public Student(String name, String emailAddress) {
-		super(name, emailAddress);
+	public Student(String name, String emailAddress, String role, String password) {
+		super(name, emailAddress, role, password);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Student(String name, String emailAddress, String userName, String password) {
-		super(name, emailAddress, userName, password);
+	public Student(String name, String emailAddress, String role) {
+		super(name, emailAddress, role);
 		// TODO Auto-generated constructor stub
 	}
+
+	
 }
