8cd0ed88e368b57cbd7251c3c8ef3c4c968b26df
Bosnyák Imre Dávid <suti211@gmail.com>
2017-03-29 13:37:44 +0200

    created userdb.xml

--- src/main/java/io/PersistentStorage.java
+++ src/main/java/io/PersistentStorage.java
@@ -1,39 +1,67 @@
 package io;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 import user.User;
 
 public class PersistentStorage {
-	
+
 	private File xmlFile;
 	private String filePath;
 	static PersistentStorage storage;
-	private Document doc;
-	
-	public PersistentStorage(){
-		
+
+	public PersistentStorage() {
+
 	}
-	
+
 	public PersistentStorage(String filePath) {
-		
+
 	}
-	
-	public PersistentStorage newInstance(){
-		if(storage == null){
+
+	public PersistentStorage newInstance() {
+		if (storage == null) {
 			storage = new PersistentStorage();
 		}
 		return storage;
 	}
+
+//	public List<User> loadUsers() throws Exception {
+//		List<User> users = new ArrayList<>();
+//		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder db = dbf.newDocumentBuilder();
+//		Document doc = db.parse(xmlFile);
+//		doc.normalize();
+//		
+//		Element root = doc.getDocumentElement();
+//		
+//	}
 	
-	public List<User> loadUsers(){
+	public void addUser(User user) throws SAXException, IOException, ParserConfigurationException{
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(xmlFile);
+		doc.normalize();
+		
+		Element root = doc.getDocumentElement();
+		
+		Element newUser = doc.createElement("user");
+		
+		
+//		root.appendChild(newChild)
+		
+		
 	}
 
 }
