d3e95d23f67c629706c4047a0dd711a258807b1f
redgyuf <faramirex070@gmail.com>
2017-03-29 11:42:37 +0200

    singleton class basic functions implemented

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -1,5 +1,48 @@
 package data;
 
-public class Data {
+import java.util.ArrayList;
 
+import user.Mentor;
+import user.Student;
+import user.User;
+
+public class Data {
+	private ArrayList<User> users = new ArrayList<User>();
+	
+	private static Data singleton = new Data( );	
+	private Data() { }
+
+	public static Data newInstance( ) {
+		return singleton;
+	}
+
+	public ArrayList<User> getUserList() {
+		return users;
+	}
+	
+	
+	public void addUser(String userType, String name, String emailAddress){
+		switch(userType){
+		case "student":
+			users.add(new Student(name, emailAddress));
+			break;
+		case "mentor":
+			users.add(new Mentor(name, emailAddress));
+			break;
+		}
+	}
+	
+	public void addUser(String userType, String name, String emailAddress, String userName, String password){
+		switch(userType){
+		case "student":
+			users.add(new Student(name, emailAddress, userName, password));
+			break;
+		case "mentor":
+			users.add(new Mentor(name, emailAddress, userName, password));
+			break;
+		}
+	}
+	
+	
+	
 }
