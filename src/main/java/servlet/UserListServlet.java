e4683eef111b6efaee7ae27f5e69ff41a4c75951
redgyuf <faramirex070@gmail.com>
2017-03-30 13:49:53 +0200

    userList servlet implemented

---
+++ src/main/java/servlet/UserListServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.Data;
+import user.User;
+
+/**
+ * Servlet implementation class UserListServlet
+ */
+public class UserListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserListServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Data data = Data.newInstance();
+		String name;
+		String email;
+		String role;
+		String tableContent = null;
+		
+		tableContent.join(""
+				, "<table>"
+				, "<tbody>"
+				,"<tr>"
+				,"<td> Name </td>"
+				,"<td> E-Mail </td>"
+				,"<td> Role </td>"
+				,"</tr>");
+		
+		
+		for (User user : data.getUserList()) {
+			name = user.getFullName();
+			email = user.getEmailAddress();
+			role = user.getRole();
+			
+			tableContent.join(""
+					,"<tr>"
+					,"<td>" + name + "</td>"
+					,"<td>" + email + "</td>"
+					,"<td>" + role + "</td>"
+					,"</tr>");
+		}
+		
+		tableContent.join(""
+				, "</tbody>"
+				, "</table>");
+		
+		request.setAttribute("fullname", data.getCurrentUser().getFullName());
+		request.setAttribute("tableContent", tableContent);
+		System.out.println("Table content generated and sent!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
