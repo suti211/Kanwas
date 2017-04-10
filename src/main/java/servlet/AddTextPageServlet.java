2f3c461a83c86ca2745d39e3a261f0f9f2258880
redgyuf <faramirex070@gmail.com>
2017-04-10 13:33:05 +0200

    removed unused code

--- src/main/java/servlet/AddTextPageServlet.java
+++ src/main/java/servlet/AddTextPageServlet.java
@@ -24,7 +24,6 @@ public class AddTextPageServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
