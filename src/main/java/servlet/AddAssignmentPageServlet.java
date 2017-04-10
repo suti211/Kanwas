e4b774abb0517c9c18f590fd15181d92ab74ff48
redgyuf <faramirex070@gmail.com>
2017-04-10 14:19:51 +0200

    minor changes

--- src/main/java/servlet/AddAssignmentPageServlet.java
+++ src/main/java/servlet/AddAssignmentPageServlet.java
@@ -30,7 +30,7 @@ public class AddAssignmentPageServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);
 		
 		String assignmentTitle = request.getParameter("assignmentTitle");
 		String assignmentContent = request.getParameter("assignmentContent");
