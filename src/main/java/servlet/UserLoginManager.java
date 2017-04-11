88620a407399ffa2e9af4ad6ea950a0450dcf462
redgyuf <faramirex070@gmail.com>
2017-04-11 11:31:31 +0200

    added isUserLoggedIn()

--- src/main/java/servlet/UserLoginManager.java
+++ src/main/java/servlet/UserLoginManager.java
@@ -1,14 +1,32 @@
 package servlet;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class UserLoginManager {
-	   private static UserLoginManager instance = null;
-	   protected UserLoginManager() {
-	      // Exists only to defeat instantiation.
-	   }
-	   public static UserLoginManager getInstance() {
-	      if(instance == null) {
-	         instance = new UserLoginManager();
-	      }
-	      return instance;
-	   }
+	
+	private boolean isUserLoggedIn(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		HttpSession session = request.getSession(false);
+		if(session == null){
+			System.out.println("No user logged in, redirected to login page...(AddTexTPageServlet)");
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
+	
+	
+   private static UserLoginManager instance = null;
+   protected UserLoginManager() {
+   }
+   public static UserLoginManager getInstance() {
+      if(instance == null) {
+         instance = new UserLoginManager();
+      }
+      return instance;
+   }
+}
