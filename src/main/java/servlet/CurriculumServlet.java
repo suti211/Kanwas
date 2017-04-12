75e8624c55eb159c1f035ba6c745638d7038ecc1
redgyuf <faramirex070@gmail.com>
2017-04-12 12:00:36 +0200

    creating new Branch

--- src/main/java/servlet/CurriculumServlet.java
+++ src/main/java/servlet/CurriculumServlet.java
@@ -7,38 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class CurriculumServlet
- */
 public class CurriculumServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private UserLoginManager LoginManager = UserLoginManager.getInstance();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CurriculumServlet() {
-        super();
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
-		
-		if(LoginManager.isUserLoggedIn(request, response)){
-		}
-		else{
-			response.sendRedirect("./login");
-		}				
+	public CurriculumServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				
 	}
 
 }
