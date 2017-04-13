381d9938c090a35fd9ccdbe06ef0627cd1ed3d1a
Bosnyák Imre Dávid <suti211@gmail.com>
2017-04-13 09:49:12 +0200

    Added minor trollling

--- src/main/java/data/GypsyModules.java
+++ src/main/java/data/GypsyModules.java
@@ -12,7 +12,7 @@ public class GypsyModules {
 	public GypsyModules() {
 		TextPage tp1 = new TextPage("Kill yourself already", "JUST DO IT!", 0, 0, true);
 		TextPage tp2 = new TextPage("Get rekt son", "WOW!", 1, 1, true);
-		TextPage tp3 = new TextPage("Daamn son", "dank memes", 2, 2, true);
+		TextPage tp3 = new TextPage("Daamn son", "<iframe width='640' height='360' src='https://www.youtube.com/embed/zqTwOoElxBA' frameborder='0' allowfullscreen></iframe>", 2, 2, true);
 		TextPage tp4 = new TextPage("Git gud", "GG WP", 3, 3, true);
 		
 		modules.add(tp1);
