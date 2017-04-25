9323de553ecdeb7e6d1e72ee9fb65b0dbf68db56
Unknown <tamasferenc90@gmail.com>
2017-04-25 11:35:55 +0200

    Gson object mapping

--- src/main/java/servlet/AddAssignmentPageServlet.java
+++ src/main/java/servlet/AddAssignmentPageServlet.java
@@ -1,5 +1,10 @@
 package servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import module.Assignment;
+import module.Module;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,10 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.sql.*;
-import java.util.ArrayList;
-import javax.servlet.*;
 
 public class AddAssignmentPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -23,6 +25,7 @@ public class AddAssignmentPageServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("post hívás");
 		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
 		Connection conn = null;
@@ -36,10 +39,10 @@ public class AddAssignmentPageServlet extends HttpServlet {
 
 		try {
 
-			String title = request.getParameter("title");
-			String content = request.getParameter("submit-text");
-			String points = request.getParameter("points");
-
+			String jsonString = request.getParameter("json");
+			System.out.println(jsonString);
+			Assignment m = new Gson().fromJson(jsonString, Assignment.class);
+			System.out.println(m);
 
 			Class.forName(driver).newInstance();
 			System.out.println("csatlakozás...");
@@ -47,11 +50,12 @@ public class AddAssignmentPageServlet extends HttpServlet {
 			conn = DriverManager.getConnection(url+dbName+unicode, "root", "ezer990");
 
 			System.out.println("csatlakozás ok");
-			PreparedStatement ps = (PreparedStatement) conn.prepareStatement("INSERT INTO `kanwas`.`assignments`(`Title`,`Content`,`MaxScore`) VALUES(?,?,?)");
+			PreparedStatement ps = (PreparedStatement) conn.prepareStatement("INSERT INTO `kanwas`.`assignments`(`Title`,`Content`,`MaxScore`,`Published`) VALUES(?,?,?,?)");
 
-			ps.setString(1,title);
-			ps.setString(2,content);
-			ps.setString(3,points);
+			ps.setString(1,m.getTitle());
+			ps.setString(2,m.getContent());
+			ps.setInt(3,m.getMaxScore());
+			ps.setBoolean(4, m.isPublished());
 
 			ps.execute();
 
