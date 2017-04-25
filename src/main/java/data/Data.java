a3776298a3454a7bde86267773d987785d18ddf8
Marcell Misó <misomarcell@gmail.com>
2017-04-25 09:43:39 +0200

    Added checkUserExist method

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -1,15 +1,17 @@
 package data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.SQLConnector;
 import module.Module;
 import user.Mentor;
 import user.Student;
 import user.User;
-import module.TextPage;
 
 public class Data {
 
@@ -18,26 +20,27 @@ public class Data {
 	private User currentUser;
 	private List<Module> modules;
 	private GypsyModules dummyList;
-	
-	private static Data singleton = new Data( );	
+
+	private SQLConnector sqlConnector = new SQLConnector();
+
+	private static Data singleton = new Data();
+
 	private Data() {
 		dummyList = new GypsyModules();
 		modules = dummyList.getModules();
 	}
 
-	public static Data newInstance( ) {
+	public static Data newInstance() {
 		return singleton;
 	}
 
 	public ArrayList<User> getUserList() {
 		return users;
 	}
-	
+
 	public User getCurrentUser(String sessionID) {
-		for (Map.Entry<User, String> sess : sessions.entrySet())
-		{
-			if (sessionID.equals(sess.getValue()))
-			{
+		for (Map.Entry<User, String> sess : sessions.entrySet()) {
+			if (sessionID.equals(sess.getValue())) {
 				return sess.getKey();
 			}
 		}
@@ -48,12 +51,12 @@ public class Data {
 		this.currentUser = currentUser;
 	}
 
-	public void addUser(String userType, String firstName, String lastName, String emailAddress){
+	public void addUser(String userType, String firstName, String lastName, String emailAddress) {
 		addUser(userType, firstName, lastName, emailAddress, null);
 	}
-	
-	public void addUser(String userType, String firstName, String lastName, String emailAddress, String password){
-		switch(userType){
+
+	public void addUser(String userType, String firstName, String lastName, String emailAddress, String password) {
+		switch (userType) {
 		case "student":
 			users.add(new Student(firstName, lastName, emailAddress, userType, password));
 			break;
@@ -62,7 +65,7 @@ public class Data {
 			break;
 		}
 	}
-	
+
 	public List<Module> getModules() {
 		return modules;
 	}
@@ -71,20 +74,37 @@ public class Data {
 		this.modules = modules;
 	}
 
-	public User getUserbyName(String name){
+	public User getUserbyName(String name) {
 		for (User user : users) {
-			if(user.getFullName().equals(name))
+			if (user.getFullName().equals(name))
 				return user;
 		}
 		return null;
 	}
-	
-	public User getUserbyEmail(String emailAddress){
+
+	public User getUserbyEmail(String emailAddress) {
 		for (User user : users) {
-			if(user.getEmailAddress().equals(emailAddress))
+			if (user.getEmailAddress().equals(emailAddress))
 				return user;
 		}
 		return null;
+	}
+
+	public boolean checkUserExist(String email) {
+		ResultSet rs = sqlConnector.getData("SELECT id FROM users WHERE email = '" + email + "' LIMIT 1");
+		try {
+
+			if (!rs.next()) {
+				return false;
+			}
+		} catch (SQLException e) {
+
+			System.out.println("Error @ RegisterServlet: Checking if user exist: \n" + e.getMessage());
+			e.printStackTrace();
+
+		}
+
+		return true;
 	}
 
 }
