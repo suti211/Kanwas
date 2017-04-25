a9fe2a9d58658bc6899e5234833dec5132c55d06
Marcell Misó <misomarcell@gmail.com>
2017-04-25 10:42:35 +0200

    Class completly rewritten (see description)
    On attempt to log in, Data class maked a new User object completley from the DB. Now, the class uses this object to validate the login. Also, getCurrentUser method added, what returns the user with the given Email address.

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -16,124 +16,112 @@ import data.Data;
 import io.SQLConnector;
 import user.User;
 
-public class LoginServlet extends HttpServlet
-{
-    /**
+public class LoginServlet extends HttpServlet {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private SQLConnector sqlConnector = new SQLConnector();
-	
+	private Data data = Data.newInstance();
+
 	String email;
-    String pass;
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        email = request.getParameter("email");
-        pass = request.getParameter("pass");
-        Data users = Data.newInstance();
-		
-        //Redirect user if already logged in
-        User currentUser = (User) request.getSession(false).getAttribute("user");
-		if (currentUser != null)
-		{
+	String pass;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		email = request.getParameter("email");
+		pass = request.getParameter("pass");
+
+		/*
+		 * Redirect user if logged in
+		 * TODO: Filter
+		 */
+		User currentUser = (User) request.getSession(false).getAttribute("user");
+		if (currentUser != null) {
 			response.sendRedirect("./profile");
 			return;
 		}
 
-		//Emmail compare
-        if (email.equals("") || pass.equals(""))
-        {
-            request.setAttribute("message", "<div class=\"error\"> ERROR: Requested field is empty.  </div>");
-        }
+		/*
+		 * Check if Password & EMail are given and not empty
+		 */
+		if (email.equals("") || pass.equals("")) {
+			request.setAttribute("message", "<div class=\"error\"> ERROR: Requested field is empty.  </div>");
+		}
 
-        
-        boolean emailFound = false;
-        for (User user: users.getUserList())
-        {      	
-            if(user.getEmailAddress().equals(email))
-            {
-            	emailFound = true;
-            	
-            	//Encrypt pass
-            	try {
-        	        pass = encrypt(pass);
-        			
-        			System.out.println("re-generated password: " + pass);
-        			
-        		} catch (Exception e) {
-        			e.printStackTrace();
-        		}
-            	
-            	
-            	/*
-            	 * SQL Get password
-            	 */
-            	ResultSet rs = sqlConnector.getData("SELECT password FROM users WHERE email = '" + email + "'");
-            	String passwordFromDB = "";
-    			
-            	try {
-            		
-            		passwordFromDB = null;
-            		
-					if( rs.next() )
-					{
-						passwordFromDB = rs.getString(1);
-					}
-				} catch (SQLException e) {
-					System.out.println("Error @ LoginServlet: Get password from DB");
-					e.printStackTrace();
-				}
-            	
-            	//Password incorrect
-                if(passwordFromDB.equals(pass))
-                {
-                    //users.setCurrentUser(user);
-                    request.setAttribute("message", "<div class=\"success\"> SUCCESS: You logged in, nigga.  </div>");
-                    
-                    HttpSession session = request.getSession(true);
-                    session.setAttribute("user", user);
-                    
-                    
-                    response.sendRedirect("./profile?first=yes");
-                    return;
-                }
-                else
-                {
-                    request.setAttribute("message", "<div class=\"error\"> ERROR: E-mail or password is incorrect. </div>");
-                }
-                
-            }
-        }
-        
-        // E-mail not found
-        if (emailFound == false)
-        {
-        	request.setAttribute("message", "<div class=\"error\"> ERROR: E-mail or password is incorrect. </div>");
-        }
-        
-        doGet(request,response);
-        //request.getRequestDispatcher("/login.jsp").forward(request, response);
-    }
+		/*
+		 * Encrypt password
+		 */
+		pass = encrypt(pass);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * Check if user exist in DB
+		 */
+		if (getCurrentUser() == null) {
+			request.setAttribute("message", "<div class=\"error\"> ERROR: Requested field is empty.  </div>");
+			doGet(request, response);
+			return;
+		}
 
-    	Data d = Data.newInstance();
-    	
-    	User currentUser = null;
-    	HttpSession session = request.getSession(false);
-    	
-    	if(session != null){
-    		currentUser = (User) request.getSession(false).getAttribute("user");
-    	}
-    	
-		if (currentUser == null)
-		{
+		/*
+		 * Check if password is correct
+		 */
+		if ( !getCurrentUser().getPassword().equals(pass) ) {
+
+			request.setAttribute("message", "<div class=\"error\"> ERROR: E-mail or password is incorrect. </div>");
+			doGet(request, response);
+			return;
+		}
+		
+		/*
+		 * User successfully logged in!
+		 */
+		request.setAttribute("message", "<div class=\"success\"> SUCCESS: You logged in, nigga.  </div>");
+		HttpSession session = request.getSession(true);
+		session.setAttribute("user", getCurrentUser());
+		response.sendRedirect("./profile?first=yes");
+		
+		
+		/*
+		 * Change the page if any error happened
+		 */
+		doGet(request, response);
+	}
+
+	private User getCurrentUser() {
+		ResultSet rs = sqlConnector.getData("SELECT Email FROM users WHERE Email = '" + email + "'");
+		User user = null;
+
+		try {
+			if (rs.next()) {
+				user = data.getUserByEmail(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error @ getCurrentUser: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return user;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		User currentUser = null;
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			currentUser = (User) request.getSession(false).getAttribute("user");
+		}
+
+		if (currentUser == null) {
 			request.setAttribute("extramenu", "Click here to log in.");
 			request.setAttribute("extraurl", "./login");
-		}else{
+		} else {
 			request.setAttribute("extramenu", currentUser.getFirstName());
 			request.setAttribute("extraurl", "./profile");
 		}
 		request.getRequestDispatcher("/login.jsp").forward(request, response);
-    }
+	}
 
 }
