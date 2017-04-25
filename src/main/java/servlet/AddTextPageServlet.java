2eeec7a4c93a1936d9b9563c93b7bb5de6cef4f7
redgyuf <faramirex070@gmail.com>
2017-04-25 13:26:30 +0200

    Access restriction added

--- src/main/java/servlet/AddTextPageServlet.java
+++ src/main/java/servlet/AddTextPageServlet.java
@@ -6,16 +6,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import io.SQLConnector;
 import module.TextPage;
+import user.User;
 
 
 public class AddTextPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private UserLoginManager LoginManager = UserLoginManager.getInstance();
 
     public AddTextPageServlet() {
         super();
@@ -25,20 +27,31 @@ public class AddTextPageServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-		String jsonString = null;
+		HttpSession session = request.getSession(false);
+		User user = (User) session.getAttribute("user");
 		
-		if(request.getParameter("json") == null){
-			System.out.println("'json' parameter is null!");
-			return;
+		if(user.getRole().equals("mentor")){
+			String jsonString = null;
+			
+			if(request.getParameter("json") == null){
+				System.out.println("'json' parameter is null!");
+				return;
+			}
+					
+			jsonString = request.getParameter("json");
+			System.out.println("JSON String: " + jsonString);
+			
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			TextPage textpage = gson.fromJson(jsonString, TextPage.class);
+			
+			SQLConnector sqlConnector = new SQLConnector();
+			String query = "";
+			
+			
+			sqlConnector.sendQuery(query);
 		}
-				
-		jsonString = request.getParameter("json");
-		System.out.println("JSON String: " + jsonString);
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		TextPage textpage = gson.fromJson(jsonString, TextPage.class);
-		
-		
+		else{
+			System.out.println("Student tried to AddNewTextPage!");
+		}		
 	}
-
 }
